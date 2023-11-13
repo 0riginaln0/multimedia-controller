24995fd62564eea9ad791b2ffb7d54e3ba793869
0riginaln0 <74508026+0riginaln0@users.noreply.github.com>
2023-11-13 18:17:27 +0300

    split buttons logic into 2 functions

--- code.py
+++ code.py
@@ -39,9 +39,7 @@ next_button_state = None
 
 # Global constants, States
 DOUBLE_TAP_TIME_LIMIT = 0.5 # in seconds
-ENCODER_WAS_PRESSED = True
-NEXT_BUTTON_WAS_PRESSED = True
-PREV_BUTTON_WAS_PRESSED = True
+WAS_PRESSED = True
 
 
 def encoder_change_volume():
@@ -65,9 +63,9 @@ def encoder_is_double_tapped(first_tap_time) -> bool:
         time_dif = time.monotonic() - first_tap_time
         is_pressed = not encoder_button.value
         if is_pressed and inner_button_state is None:
-            inner_button_state = ENCODER_WAS_PRESSED
+            inner_button_state = WAS_PRESSED
         is_released = encoder_button.value
-        if inner_button_state == ENCODER_WAS_PRESSED and is_released:
+        if inner_button_state == WAS_PRESSED and is_released:
             return True
         if time_dif > DOUBLE_TAP_TIME_LIMIT:
             return False
@@ -77,42 +75,41 @@ def encoder_change_mute_or_playback_state():
     global encoder_button_state
     is_pressed = not encoder_button.value
     if is_pressed and encoder_button_state is None:
-        encoder_button_state = ENCODER_WAS_PRESSED
+        encoder_button_state = WAS_PRESSED
     is_released = encoder_button.value
-    if encoder_button_state == ENCODER_WAS_PRESSED and is_released:
+    if encoder_button_state == WAS_PRESSED and is_released:
         first_tap_time = time.monotonic()
         if encoder_is_double_tapped(first_tap_time):
             cc.send(ConsumerControlCode.MUTE)
         else:
             cc.send(ConsumerControlCode.PLAY_PAUSE)
         encoder_button_state = None
-        
 
-def buttons_previous_next_track():
+def next_track_button():
     global next_button_state
-    global previous_button_state
     is_next_pressed = not next_scan_button.value
-    is_previous_pressed = not previous_scan_button.value
-
     if is_next_pressed and next_button_state is None:
-        next_button_state = NEXT_BUTTON_WAS_PRESSED
-    elif is_previous_pressed and previous_button_state is None:
-        previous_button_state = PREV_BUTTON_WAS_PRESSED
-
+        next_button_state = WAS_PRESSED
     is_next_released = next_scan_button.value
-    is_previous_released = previous_scan_button.value
-
-    if (next_button_state is not None and is_next_released) or (previous_button_state is not None and is_previous_released):  # Fix the variable name here
-        if next_button_state == NEXT_BUTTON_WAS_PRESSED:
-            cc.send(ConsumerControlCode.SCAN_NEXT_TRACK)
-        elif previous_button_state == PREV_BUTTON_WAS_PRESSED:
-            cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK)
-
+    if next_button_state == WAS_PRESSED and is_next_released:
+        cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK)
         next_button_state = None
+
+
+def previous_track_button():
+    global previous_button_state
+    is_previous_pressed = not previous_scan_button.value
+    if is_previous_pressed and previous_button_state is None:
+        previous_button_state = WAS_PRESSED
+    is_previous_released = previous_scan_button.value
+    if previous_button_state == WAS_PRESSED and is_previous_released:
+        cc.send(ConsumerControlCode.SCAN_NEXT_TRACK)
         previous_button_state = None
+
 
 while True:
     encoder_change_volume()
     encoder_change_mute_or_playback_state()
-    buttons_previous_next_track()
+    next_track_button()
+    previous_track_button()
 
